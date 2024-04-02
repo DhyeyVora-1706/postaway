bdc47345baa8491d4ef013165f118a17b6c9e458
Dhyey Vora <dhyey1706@gmail.com>
2024-04-02 12:29:59 +0530

    index.js commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const server = express();
 
-server.listen(4000,()=>{
-    console.log('Server is running on 4000');
-});
+server.listen(4500,function(){
+    console.log("Server is running on port 4500");
+})
